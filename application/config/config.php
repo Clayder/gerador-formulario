28dd6a3ff86d1e2e4fd3c3f6424738dbad046d94
Peter Clayder <peterclayder@gmail.com>
2017-05-20 14:35:25 +0000

    config.php alterados online com o Bitbucket

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,6 @@
 <?php
 
-define("BASE_URL", "http://localhost/www/peterdev/framework-form/");
-//define("BASE_URL", "http://www.pxsti.com.br/uff/framework-form/");
+define("BASE_URL", "http://localhost/www/framework-form/"); // inserir a / no final da url
 define("PAGINA_INICIAL", "home");
-define("PASTA_BASE_VIEW", "C:\\xampp\\htdocs\\www\\peterdev\\framework-form\\application\\view\\");
-//define("PASTA_BASE_VIEW", "application/view/");
+define("PASTA_BASE_VIEW", "application/view/"); // inserir a / no final da url
+
